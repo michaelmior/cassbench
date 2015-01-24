d46ae64348710bbca2bf219c2f739a8699565b28
Michael Mior <mmior@uwaterloo.ca>
2015-01-24 08:29:18 -0500

    Fix number of iterations

--- lib/cassbench.rb
+++ lib/cassbench.rb
@@ -1,6 +1,15 @@
 require 'benchmark/ips'
 require 'benchmark/suite'
 
+module Benchmark
+  module IPS
+    class Job
+      # Microseconds per minutes (this forces more iterations)
+      MICROSECONDS_PER_100MS = 60_000_000
+    end
+  end
+end
+
 module CassBench
   class Bench
     @client = nil
@@ -9,7 +18,7 @@ module CassBench
       suite = Benchmark::Suite.create do |suite|
 
         Benchmark.ips do |bench|
-          bench.config time: 5
+          bench.config warmup: 30, time: 300
           block.call bench, @session
         end
       end
