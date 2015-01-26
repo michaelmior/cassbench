0c6cedaf4fe9c867d0d0e4c0a7cbba90543c00be
Michael Mior <mmior@uwaterloo.ca>
2015-01-26 13:03:05 -0500

    Insert other rows for single select test

--- bench/single_row_fetch.rb
+++ bench/single_row_fetch.rb
@@ -1,9 +1,18 @@
 CassBench::Bench.run do |bench, session|
-  session.execute "CREATE TABLE test (id uuid PRIMARY KEY, data text);"
-  session.execute "INSERT INTO test (id, data) VALUES " \
-                  "(756716f7-2e54-4715-9f00-91dcbea6cf50, " \
-                  "'11111111111111111111111111111111111111111111111111" \
-                  "11111111111111111111111111111111111111111111111111');"
+  session.execute "CREATE TABLE test (id uuid PRIMARY KEY, data text) "\
+                  "WITH caching = 'ALL';"
+
+  data = '1' * 100
+  insert = session.prepare "INSERT INTO test (id, data) VALUES (?, ?)"
+  generator = Cassandra::Uuid::Generator.new
+
+  # Insert 100,000 random rows
+  1.upto(100_000) do
+    session.execute insert, generator.uuid, data
+  end
+  session.execute insert,
+                  Cassandra::Uuid.new('756716f7-2e54-4715-9f00-91dcbea6cf50'),
+                  data
 
   bench.report('fetch') do |times|
     i = 0
