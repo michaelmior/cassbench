4117abcf886a771e7ec40f4af432c1a0e12dd75b
Michael Mior <mmior@uwaterloo.ca>
2015-01-27 13:10:10 -0500

    Stop using UUIDs

--- bench/column_fetch.rb
+++ bench/column_fetch.rb
@@ -1,21 +1,17 @@
 class ColumnFetch < CassBench::Bench
   def self.setup(session, options)
-    session.execute "CREATE TABLE column_fetch (id uuid, col text, " \
+    session.execute "CREATE TABLE column_fetch (id text, col text, " \
                     "data text, PRIMARY KEY (id, col)) " \
                     "WITH caching = '#{options[:caching]}';"
 
     data = '1' * 100
     insert = session.prepare "INSERT INTO column_fetch (id, col, data) " \
                              "VALUES (?, ?, ?)"
-    generator = Cassandra::Uuid::Generator.new
 
     # Insert 100,000 random rows
     1.upto(100_000) do |i|
-      session.execute insert, generator.uuid, '%010d' % i, data
+      session.execute insert, '%010d' % i, '%010d' % i, data
     end
-    session.execute insert,
-      Cassandra::Uuid.new('756716f7-2e54-4715-9f00-91dcbea6cf50'), '%010d' % 1,
-      data
   end
 
   def self.run(bench, session)
@@ -24,7 +20,7 @@ class ColumnFetch < CassBench::Bench
       futures = []
       while i < times
         query = "SELECT data FROM column_fetch WHERE " \
-                "id=756716f7-2e54-4715-9f00-91dcbea6cf50 AND col='0';"
+                "id='0000000001' AND col='0000000001';"
         futures.push session.execute_async(query)
         i += 1
       end
--- bench/single_row_fetch.rb
+++ bench/single_row_fetch.rb
@@ -1,21 +1,17 @@
 class SingleRowFetch < CassBench::Bench
   def self.setup(session, options)
     session.execute "CREATE TABLE single_row_fetch " \
-                    "(id uuid PRIMARY KEY, data text) " \
+                    "(id text PRIMARY KEY, data text) " \
                     "WITH caching = '#{options[:caching]}';"
 
     data = '1' * 100
     insert = session.prepare "INSERT INTO single_row_fetch (id, data) " \
                              "VALUES (?, ?)"
-    generator = Cassandra::Uuid::Generator.new
 
     # Insert 100,000 random rows
     1.upto(100_000) do
-      session.execute insert, generator.uuid, data
+      session.execute insert, '%010d' % i, data
     end
-    session.execute insert,
-      Cassandra::Uuid.new('756716f7-2e54-4715-9f00-91dcbea6cf50'),
-      data
   end
 
   def self.run(bench, session)
@@ -23,8 +19,7 @@ class SingleRowFetch < CassBench::Bench
       i = 0
       futures = []
       while i < times
-        query = "SELECT data FROM single_row_fetch WHERE " \
-                "id=756716f7-2e54-4715-9f00-91dcbea6cf50;"
+        query = "SELECT data FROM single_row_fetch WHERE id='0000000001';"
         futures.push session.execute_async(query)
         i += 1
       end
