b9d6e2664b8818e60d3334c6b7bec6625b356b3e
Michael Mior <mmior@uwaterloo.ca>
2015-01-28 20:30:45 -0500

    Refactor JMX commands

--- lib/cassbench.rb
+++ lib/cassbench.rb
@@ -34,19 +34,10 @@ module CassBench
 
       # Optionally flush or compact the keyspace after setup
       if options[:flush] || options[:compact]
-        cluster.hosts.each do |host|
-          conn = JMX::MBean.create_connection host: host.ip.to_s, port: 7199
-          sproxy = JMX::MBean.find_by_name 'org.apache.cassandra.db:'\
-                                           'type=StorageService',
-                                           connection: conn
+        jmx_command cluster, :force_keyspace_flush, options[:keyspace] \
           if options[:flush]
-            sproxy.force_keyspace_flush options[:keyspace], [].to_java(:string)
-          end
+        jmx_command cluster, :force_keyspace_compaction, options[:keyspace] \
           if options[:compact]
-            sproxy.force_keyspace_compaction options[:keyspace],
-                                             [].to_java(:string)
-          end
-        end
       end
 
       # Run all the benchmarks
@@ -62,6 +53,17 @@ module CassBench
 
       # Run the cleanup for each benchmark
       benchmarks.each { |benchmark| benchmark.cleanup session }
+    end
+
+    # Send a management command to all nodes in a cluster
+    def self.jmx_command(cluster, command, keyspace)
+      cluster.hosts.each do |host|
+        conn = JMX::MBean.create_connection host: host.ip.to_s, port: 7199
+        sproxy = JMX::MBean.find_by_name 'org.apache.cassandra.db:'\
+                                         'type=StorageService',
+                                         connection: conn
+        sproxy.public_send command, keyspace, [].to_java(:string)
+      end
     end
   end
 end
