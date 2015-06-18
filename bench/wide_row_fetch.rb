24726a8e8bcd8a2e1c13249264e5e11f549d2cd2
Michael Mior <mmior@uwaterloo.ca>
2015-06-18 16:14:10 -0400

    Fix wide_row_fetch for batch random

--- bench/wide_row_fetch.rb
+++ bench/wide_row_fetch.rb
@@ -1,6 +1,6 @@
 class WideRowFetch < CassBench::Bench
   def self.setup(cluster, session, options)
-    session.execute "CREATE TABLE wide_row_fetch (id uuid, col uuid, " \
+    session.execute "CREATE TABLE wide_row_fetch (id text, col text, " \
                     "data text, PRIMARY KEY (id, col)) " \
                     "WITH caching = '#{options[:caching]}' AND " \
                     "compression={'sstable_compression': " \
@@ -14,26 +14,35 @@ class WideRowFetch < CassBench::Bench
                              "VALUES (?, ?, ?)"
 
     # Insert random rows
+    @@indexes = 1.upto([options[:rows], options[:columns]].max).map do
+      |i| '%010d' % i
+    end
     options[:overwrite].times do
       1.upto(options[:rows]) do |i|
         1.upto(options[:columns]) do |j|
-          session.execute insert, Cassandra::Uuid.new(i),
-                                  Cassandra::Uuid.new(j), data
+          session.execute insert, @@indexes[i - 1],
+                                  @@indexes[j - 1], data
         end
         self.jmx_command cluster, :force_keyspace_flush, options[:keyspace] \
           if options[:flush_every] > 0 && (i % options[:flush_every] == 0)
       end
-    end
+    end if options[:create]
 
-    @@indexes = 1.upto(options[:rows]).to_a.shuffle.map do |n|
-      Cassandra::Uuid.new(options[:random] ? n : 1)
+    if options[:batch]
+      @@query = "SELECT data FROM wide_row_fetch WHERE id IN ?;"
+    else
+      @@query = "SELECT data FROM wide_row_fetch WHERE id=?;"
     end
-    @@query = session.prepare "SELECT data FROM wide_row_fetch WHERE id=?;"
   end
 
   def self.run(times, session, options)
-    0.upto(times - 1) do |i|
-      session.execute @@query, @@indexes[i % options[:rows]]
+    # Start at a random offset
+    start = rand options[:rows]
+
+    if options[:batch]
+      if options[:random]
+        session.execute(@@query, @@indexes.sample(times)).each(&:itself)
+      end
     end
   end
 
